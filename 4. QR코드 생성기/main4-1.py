b080cf2bd99cb8735fcbc459708c65352f795bd8
ri-naa <121746871+ri-naa@users.noreply.github.com>
2023-02-27 15:47:19 +0900

    Delete 4. QR코드 생성기 directory

--- 4. QR코드 생성기/main4-1.py
+++
@@ -1,6 +0,0 @@
-import qrcode
-
-qr_data = 'www.naver.com'
-qr_img = qrcode.make(qr_data) #qrcode,make 로 이미지를 만들어 qr_img 변수에 바인딩 한다.
-save_path = '4. QR코드 생성기\\' + qr_data + '.png'
-qr_img.save(save_path)
