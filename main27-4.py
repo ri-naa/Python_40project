802c299d0534da70778ba96a2ecbbec2f3ed1b8c
ri-naa <121746871+ri-naa@users.noreply.github.com>
2023-03-02 19:13:14 +0900

    Add files via upload

---
+++ main27-4.py
@@ -0,0 +1,10 @@
+import folium
+
+
+map = folium.Map(location=[37,126],zoom_start=7)
+
+marker = folium.Marker([37.560311827, 126.944695670], popup='이화여자대학교', icon = folium.Icon(color='green'))
+
+marker.add_to(map)
+
+map.save(r'27. 전국의 대학교 위치 시각화하기\uni_map.html')
